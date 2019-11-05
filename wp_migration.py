c8214db997bdedbd618419402306ed07c376ab39
Mark Hayhurst <Mark.Hayhurst@Gamesys.co.uk>
2019-11-05 09:39:50 +0000

    Updated code

--- wp_migration.py
+++ wp_migration.py
@@ -2,6 +2,10 @@
 
 import mysql.connector
 from slugify import slugify
+import datetime
+import time
+
+import wp_crypt
 
 HOST = "192.196.156.250"
 HOST = "127.0.0.1"
@@ -13,6 +17,9 @@ SOURCE_DATABASE = "ocdev_saa_staging"
 TARGET_USER = "iamwp_dev"
 TARGET_PASSWORD = "J!2hFvaAKIq4"
 TARGET_DATABASE = "iamwp_dev"
+
+SOURCE_DB = None
+TARGET_DB = None
 
 MY_CACHE = None
 
@@ -38,15 +45,15 @@ def connect(host, user, password, database):
     return db
 
 
-def migrate_users(source_db, target_db):
+def migrate_users():
 
     # delete existing data
 
     # fetch data
 
-    my_cursor = source_db.cursor()
+    my_cursor = SOURCE_DB.cursor()
 
-    sql = "SELECT * FROM Artists LIMIT 100"
+    sql = "SELECT * FROM Artists ORDER BY Code LIMIT 10"
     my_cursor.execute(sql)
 
     my_result = my_cursor.fetchall()
@@ -57,9 +64,14 @@ def migrate_users(source_db, target_db):
     # insert wp_users
 
 
-def execute_sql(db, sql, args, commit=False, get_last_id=False):
+def execute_sql(db, sql, args, commit=False, get_results=True, get_last_id=False):
 
-    my_cursor = db.cursor()
+    my_cursor = db.cursor(dictionary=True)
+
+    # print(sql)
+    # print(args)
+    # if args:
+    #     print(sql % args)
 
     my_cursor.execute(sql, args)
 
@@ -68,42 +80,243 @@ def execute_sql(db, sql, args, commit=False, get_last_id=False):
 
     if get_last_id:
         return my_cursor.lastrowid
-    else:
+    elif get_results:
         return my_cursor.fetchall()
 
 
-def create_wp_user(r):
+def get_acf_field_code(field_name):
+
+    global TARGET_DB
+
+    args = {"post_excerpt": field_name}
+    sql = "SELECT post_name FROM wp_posts WHERE post_excerpt=%(post_excerpt)s"
+    results = execute_sql(TARGET_DB, sql, args, False, get_results=True)
+    return results[0]['post_name']
+
+
+def xstr(s):
+    return '' if s is None else str(s)
+
+
+def create_user(r):
+
+    global TARGET_DB
+    t = dict()
+
+    print(r['Code'])
+
+    t['user_login'] = r['Code']
 
     # delete existing from wp_usermeta
 
-    sql = "DELETE FROM wp_usermeta WHERE user_id in (SELECT ID FROM wp_users WHERE user_login=%s)"
-    execute_sql(TARGET_DB, sql, args=r['Code'])
+    sql = "DELETE FROM wp_usermeta WHERE user_id in (SELECT ID FROM wp_users WHERE user_login=%(user_login)s)"
+    execute_sql(TARGET_DB, sql, t, commit=True, get_results=False)
 
     # delete existing from wp_users
 
-    sql = "DELETE FROM wp_users WHERE user_login=%s"
-    execute_sql(TARGET_DB, sql, args=r['Code'])
+    sql = "DELETE FROM wp_users WHERE user_login=%(user_login)s"
+    execute_sql(TARGET_DB, sql, t, commit=True, get_results=False)
 
     # insert new in wp_users
 
     sql = ("INSERT INTO wp_users"
            " (user_login, user_pass, user_nicename, user_email, user_url, user_registered, user_activation_key, user_status, display_name)"
-           "VALUES %(user_login)s, %(user_pass)s, %(user_nicename)s, %(user_url)s, %(user_registered)s, %(user_activation_key)s, %user_status)s, %(display_name)s")
+           " VALUES(%(user_login)s, %(user_pass)s, %(user_nicename)s, %(user_email)s, %(user_url)s, %(user_registered)s, %(user_activation_key)s, %(user_status)s, %(display_name)s)")
 
-    t = dict()
     t['user_login'] = r["Code"]
-    t['user_pass'] = ''
+    # password = "PASSWORD".encode('utf-8')
+    # t['user_pass'] = password # wp_crypt.crypt_private(r['Password'].encode('utf-8'))
+    t['user_pass'] = '' # TODO: sort out password generation
     t['user_nicename'] = r["Code"]
-    t['user_email'] = r["EmailAddress"]
-    t['user_url'] = ''
-    t['user_registered'] = r["Created"]
+    t['user_email'] = r['Code'] + "@ubiservers.net"
+    t['user_url'] = xstr(r['WebAddress'])
+    t['user_registered'] = str(r["Created"])
     t['user_activation_key'] = ''
     t['user_status'] = 0
     t['display_name'] = r["Code"]
 
-    ID = execute_sql(target_db, sql, t, commit=True, get_last_id=True)
+    USER_ID = execute_sql(TARGET_DB, sql, t, commit=True, get_last_id=True)
 
     # insert new in wp_usermeta
+
+    wp_user_meta_rows = dict()
+
+    wp_user_meta_rows['nickname'] = r['Code']
+    wp_user_meta_rows['first_name'] = r['FirstNames']
+    wp_user_meta_rows['billing_first_name'] = r['FirstNames']
+    wp_user_meta_rows['shipping_first_name'] = r['FirstNames']
+    wp_user_meta_rows['last_name'] = r['FamilyName']
+    wp_user_meta_rows['billing_last_name'] = r['FamilyName']
+    wp_user_meta_rows['shipping_last_name'] = r['FamilyName']
+    wp_user_meta_rows['description'] = ''
+    wp_user_meta_rows['rich_editing'] = 'true'
+    wp_user_meta_rows['syntax_highlighting'] = 'true'
+    wp_user_meta_rows['comment_shortcuts'] = 'false'
+    wp_user_meta_rows['admin_color'] = 'fresh'
+    wp_user_meta_rows['use_ssl'] = '0'
+    wp_user_meta_rows['show_admin_bar_front'] = 'true'
+    wp_user_meta_rows['locale'] = ''
+    wp_user_meta_rows['wp_capabilities'] = 'a:2:{s:6:"author";b:1;s:6:"artist";b:1;}'
+    wp_user_meta_rows['wp_user_level'] = '2'
+    wp_user_meta_rows['dismissed_wp_pointers'] = ''
+
+    wp_user_meta_rows['billing_company'] = r['Organisation']
+    wp_user_meta_rows['shipping_company'] = r['Organisation']
+
+    wp_user_meta_rows['billing_address_1'] = r['PostalAddress']
+    wp_user_meta_rows['billing_address_2'] = ''
+    wp_user_meta_rows['billing_city'] = r['PostalTown']
+    wp_user_meta_rows['billing_postcode'] = r['PostalCode']
+    wp_user_meta_rows['billing_state'] = '' # TODO: lookup state code
+    wp_user_meta_rows['billing_country'] = 'ZA' # TODO: lookup country code
+
+    wp_user_meta_rows['shipping_address_1'] = r['StreetAddress1']
+    wp_user_meta_rows['shipping_address_2'] = r['StreetAddress2']
+    wp_user_meta_rows['shipping_city'] = r['Town']
+    wp_user_meta_rows['shipping_postcode'] = r['PostCode']
+    wp_user_meta_rows['shipping_state'] = '' # TODO: lookup state code
+    wp_user_meta_rows['shipping_country'] = 'ZA'
+
+    wp_user_meta_rows['description'] = r['Exhibitions']
+
+    wp_user_meta_rows['artist_id'] = r['ID']
+    wp_user_meta_rows['_artist_id'] = get_acf_field_code('artist_id')
+
+    wp_user_meta_rows['artist_mobile_phone'] = r['MobilePhone']
+    wp_user_meta_rows['_artist_mobile_phone'] = get_acf_field_code('artist_mobile_phone')
+
+    wp_user_meta_rows['artist_work_phone'] = r['WorkPhone']
+    wp_user_meta_rows['_artist_work_phone'] = get_acf_field_code('artist_work_phone')
+
+    wp_user_meta_rows['artist_fax'] = r['Fax']
+    wp_user_meta_rows['_artist_fax'] = get_acf_field_code('artist_fax')
+
+    wp_user_meta_rows['artist_region'] = r['Region']
+    wp_user_meta_rows['_artist_region'] = get_acf_field_code('artist_region')
+
+    wp_user_meta_rows['artist_artist_notes'] = r['ArtistNotes']
+    wp_user_meta_rows['_artist_artist_notes'] = get_acf_field_code('artist_artist_notes')
+
+    wp_user_meta_rows['artist_education'] = r['Education']
+    wp_user_meta_rows['_artist_education'] = get_acf_field_code('artist_education')
+
+    wp_user_meta_rows['artist_preferred_media'] = r['PreferredMedia']
+    wp_user_meta_rows['_artist_preferred_media'] = get_acf_field_code('artist_preferred_media')
+
+    wp_user_meta_rows['artist_promotion_authorised'] = r['PromotionAuthorised']
+    wp_user_meta_rows['_artist_promotion_authorised'] = get_acf_field_code('artist_promotion_authorised')
+
+    wp_user_meta_rows['artist_vat_registered'] = r['VATRegistered']
+    wp_user_meta_rows['_artist_vat_registered'] = get_acf_field_code('artist_vat_registered')
+
+    wp_user_meta_rows['artist_deceased'] = r['Deceased']
+    wp_user_meta_rows['_artist_deceased'] = get_acf_field_code('artist_deceased')
+
+    wp_user_meta_rows['artist_accepts_commissions'] = r['AcceptsCommissions']
+    wp_user_meta_rows['_artist_accepts_commissions'] = get_acf_field_code('artist_accepts_commissions')
+
+    wp_user_meta_rows['artist_seo_slug'] = r['SEOFriendly']
+    wp_user_meta_rows['_artist_seo_slug'] = get_acf_field_code('artist_seo_slug')
+
+    wp_user_meta_rows['artist_editor_notes'] = r['EditorNotes']
+    wp_user_meta_rows['_artist_editor_notes'] = get_acf_field_code('artist_editor_notes')
+
+    wp_user_meta_rows['_order_count'] = '0'
+    wp_user_meta_rows['last_update'] = str(int(time.mktime(datetime.datetime.now().timetuple())))
+
+    for c in wp_user_meta_rows:
+        t['user_id'] = USER_ID
+        t['meta_key'] = c
+        t['meta_value'] = wp_user_meta_rows[c]
+
+        sql = ("INSERT INTO wp_usermeta"
+               " (user_id, meta_key, meta_value)"
+               "VALUES(%(user_id)s, %(meta_key)s, %(meta_value)s)")
+        UMETA_ID = execute_sql(TARGET_DB, sql, t, commit=True, get_last_id=True)
+
+
+def migrate_categories(my_source_db, my_target_db, table, label):
+    my_source_cursor = my_source_db.cursor()
+    my_target_cursor = my_target_db.cursor()
+
+    # get parent id
+
+    sql = "SELECT term_id FROM wp_terms where name='%s' " % label
+    results = execute_sql(my_target_db, sql, '')
+    group_id = results[0][0]
+    parent = group_id
+
+    # get source data
+
+    sql = "SELECT * FROM %s ORDER BY ID" % table
+
+    results = execute_sql(my_source_db, sql, '')
+
+    # create individual rows
+
+    wp_terms_args = dict()
+    wp_termmeta_args = dict()
+    wp_term_taxonomy_args = dict()
+
+    wp_terms_args['term_group'] = 0
+    wp_term_taxonomy_args['taxonomy'] = 'product_cat'
+    wp_term_taxonomy_args['parent'] = parent
+    wp_term_taxonomy_args['count'] = 0
+
+    for r in results:
+        print(r)
+        wp_terms_sql = "INSERT INTO wp_terms (name,slug,term_group) VALUES (%(name)s,%(slug)s,%(term_group)s)"
+        wp_terms_args['name'] = r[1]
+        wp_terms_args['slug'] = slugify(r[1])
+        print(wp_terms_args)
+        term_id = execute_sql(my_target_db, wp_terms_sql, wp_terms_args, commit=True, get_last_id=True)
+
+        wp_termmeta_args['term_id'] = term_id
+        wp_termmeta_args['order'] = 0
+        wp_termmeta_args['display_type'] = ''
+        wp_termmeta_args['thumbnail_id'] = 0
+        for meta_key in ['order', 'display_type', 'thumbnail_id']:
+            wp_termmeta_args['meta_key'] = meta_key
+            wp_termmeta_args['meta_value'] = wp_termmeta_args[meta_key]
+            wp_termmeta_sql = "INSERT INTO wp_termmeta (term_id,meta_key,meta_value) VALUES (%(term_id)s,%(meta_key)s,%(meta_value)s)"
+            print(wp_termmeta_args)
+            meta_id = execute_sql(my_target_db, wp_termmeta_sql, wp_termmeta_args, commit=True, get_last_id=True)
+
+        wp_term_taxonomy_args['term_id'] = term_id
+        wp_term_taxonomy_args['description'] = ''  # needs to be blank
+        wp_term_taxonomy_sql = "INSERT INTO wp_term_taxonomy (term_id,taxonomy,description,parent,count) VALUES (%(term_id)s,%(taxonomy)s,%(description)s,%(parent)s,%(count)s)"
+        print(wp_term_taxonomy_args)
+        term_taxonomy_id = execute_sql(my_target_db, wp_term_taxonomy_sql, wp_term_taxonomy_args, commit=True,
+                                       get_last_id=True)
+
+
+def DEPR_migrate_users():
+    global SOURCE_DB
+
+    # delete existing data
+
+    # fetch data
+
+    sql = "SELECT * FROM Artists ORDER BY Code LIMIT 10"
+    my_results = execute_sql(SOURCE_DB, 'SELECT * FROM Artists LIMIT 10', '')
+
+    for r in my_results:
+        wp_user_meta_rows['artist_work_phone'] = r['WorkPhone']
+    wp_user_meta_rows['_artist_work_phone'] = get_acf_field_code('artist_work_phone')
+
+    wp_user_meta_rows['_order_count'] = '0'
+    wp_user_meta_rows['last_update'] = str(int(time.mktime(datetime.datetime.now().timetuple())))
+
+    for c in wp_user_meta_rows:
+        t['user_id'] = USER_ID
+        t['meta_key'] = c
+        t['meta_value'] = wp_user_meta_rows[c]
+
+        sql = ("INSERT INTO wp_usermeta"
+               " (user_id, meta_key, meta_value)"
+               "VALUES(%(user_id)s, %(meta_key)s, %(meta_value)s)")
+        UMETA_ID = execute_sql(TARGET_DB, sql, t, commit=True, get_last_id=True)
+
 
 
 def migrate_categories(my_source_db, my_target_db, table, label):
@@ -163,30 +376,37 @@ def migrate_categories(my_source_db, my_target_db, table, label):
 
 def migrate_users():
 
+    global SOURCE_DB
+
     # delete existing data
 
     # fetch data
 
-    my_cursor = source_db.cursor()
+    sql = "SELECT * FROM Artists ORDER BY Code LIMIT 10"
+    my_results = execute_sql(SOURCE_DB, 'SELECT * FROM Artists ORDER BY Code', '')
 
-    sql = "SELECT * FROM Artists LIMIT 100"
-    my_cursor.execute(source_db, sql)
+    for r in my_results:
+        create_user(r)
+        pass
 
-    my_result = my_cursor.fetchall()
 
-    for r in my_result:
-        create_wp_user(r)
-        # insert wp_usermeta(r)
-        print(r)
+def initial_values():
+
+    pass
 
 
 def main():
 
-    source_db = connect(HOST, SOURCE_USER, SOURCE_PASSWORD, SOURCE_DATABASE)
-    target_db = connect(HOST, TARGET_USER, TARGET_PASSWORD, TARGET_DATABASE)
+    global SOURCE_DB
+    global TARGET_DB
 
-    r = execute_sql(source_db, 'SELECT * FROM Artists LIMIT 10', '')
-    print(r)
+    SOURCE_DB = connect(HOST, SOURCE_USER, SOURCE_PASSWORD, SOURCE_DATABASE)
+    TARGET_DB = connect(HOST, TARGET_USER, TARGET_PASSWORD, TARGET_DATABASE)
+
+    # r = execute_sql(SOURCE_DB, 'SELECT * FROM Artists LIMIT 10', '')
+    # print(r)
+
+    migrate_users()
 
     # migrate_categories(source_db, target_db, "WorksCategory", "Category")
     # migrate_categories(source_db, target_db, "WorksSubject", "Subject")
